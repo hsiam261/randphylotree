29d814805eb666da2e92e400e9b6fef9673e5f91
hsiam261 <hsiam261@gmail.com>
2023-08-27 00:29:36 +0600

    fix build process
    The library was installing in the wrong directory which caused the build
    to fail.

--- build.sh
+++ build.sh
@@ -3,16 +3,22 @@ set -xe
 
 git submodule update --init --recursive
 
+LIB_ROOT=$(pwd)/lib
+mkdir -p $LIB_ROOT
+
 cd external-libs/yaml-cpp
 mkdir -p build
 cd build 
 
+# install yaml-cpp locally in ${LIB_ROOT}/yaml-cpp directory
+# ${LIB_ROOT}/yaml-cpp must be in CMAKE_PREFIX_LIST
+# for build to work
 if [ $1 == "RELEASE" ]
 then
-	cmake .. -DCMAKE_INSTALL_PREFIX=../../lib/yaml-cpp -DCMAKE_BUILD_TYPE=Release
+	cmake .. -DCMAKE_INSTALL_PREFIX=${LIB_ROOT}/yaml-cpp -DCMAKE_BUILD_TYPE=Release
 	cmake --build .
 else
-	cmake .. -DCMAKE_INSTALL_PREFIX=../../lib/yaddml-cpp
+	cmake .. -DCMAKE_INSTALL_PREFIX=${LIB_ROOT}/yaml-cpp
 	cmake --build . --target install
 fi
 
