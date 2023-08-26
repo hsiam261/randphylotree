a39d499ef875d44bcbc8a0e2a1202bc3c3fb4cfc
hsiam261 <hsiam261@gmail.com>
2023-08-26 19:10:59 +0600

    add option enabling compiler optimization

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+set -xe
 
 git submodule update --init --recursive
 
@@ -6,10 +7,27 @@ cd external-libs/yaml-cpp
 mkdir -p build
 cd build 
 
-cmake .. -DCMAKE_INSTALL_PREFIX=../../lib/yaml-cpp
-cmake --build . --target install
+if [ $1 == "RELEASE" ]
+then
+	cmake .. -DCMAKE_INSTALL_PREFIX=../../lib/yaml-cpp -DCMAKE_BUILD_TYPE=Release
+	cmake --build .
+else
+	cmake .. -DCMAKE_INSTALL_PREFIX=../../lib/yaddml-cpp
+	cmake --build . --target install
+fi
 
 cd ../../../ 
-cmake .
-make
+
+if [ $1 == "RELEASE" ]
+then
+	# Generate build files for the "Release" configuration
+	mkdir -p release
+	cd release
+	cmake -DCMAKE_BUILD_TYPE=Release ..
+	# Build the project for the "Release" configuration
+	make
+else
+	cmake .
+	make
+fi
 
